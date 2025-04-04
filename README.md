dd57016bc110e543f8f09cc566ee28439f4fbefc
Naksh Prajapati <thenakshprajapat@outlook.com>
2025-04-04 11:53:20 +0530

    Initial commit: Grocery Store App

---
+++ README.md
@@ -0,0 +1,17 @@
+ Spicy Grocery Store 🛒🌶️
+
+This is a fun Python CLI app where you can search for electronic items and play mini discount games to get lucky deals!  
+
+## 🔥 Features
+- Search for items like MacBook, Phone, etc.
+- Two discount modes:
+  - 🎯 **Luck Discount**: Random discount between 1% to 20%
+  - 🎲 **Random Discount**: Random discount up to 100%, but costs ₹1000 to play!
+- Price calculator with final cart value
+
+## 🚀 How to Run
+1. Make sure you have Python installed (3.x)
+2. Clone this repo or download the file
+3. Run it using:
+```bash
+python grocery_store.py
---
+++ grocery_store.py
@@ -0,0 +1,44 @@
+import random
+item = input("Enter item you are looking for : ")
+item_list = ['macbook','phone','surface go','boat headphones','pixelwatch']
+item_dict = {'macbook':100000 , 'phone': 52000 , 'surface go' : 120000 , 'boat headphones' : 2000 , 'pixel watch' : 30000}
+luck_disc = random.randint(1,20)
+rand_disc = random.randint(0,100)
+luck_disc1 = luck_disc/100
+rand_disc1 = rand_disc/100 
+if item.lower() in item_list:
+    print("Item Found")
+    print("Price of ",item,"is ",item_dict[item.lower()])
+    print("Do you want to play a game for discount?")
+    ch = input("Type y or n ! : ")
+    if ch.lower() == "y":
+        print("There are two type of discounts \nOne is Luck\nAnother is Random")
+        ch1 = input("Enter your choice [luck or random]: ")
+        if ch1.lower() == "luck":
+            print("you get a discount of : ",luck_disc,"%")
+            print("Your total value is now : ",item_dict[item.lower()])
+            print("Your disc = ",item_dict[item.lower()]*luck_disc1)
+            print("Your cart value is : ",item_dict[item.lower()]-item_dict[item.lower()]*luck_disc1)
+        elif ch1.lower() == "random":
+            print("You will be charged 1000 rupees for this")
+            ch2 = input("Type y or n : ")
+            if ch2.lower() == "y":
+                print("You get a discount of : ",rand_disc,"%")
+                print("Your total value is now : ",item_dict[item.lower()])
+                print("Charge for random discount opt : 1000")
+                print("Your disc = ",item_dict[item.lower()]*rand_disc1)
+                print("Your cart value is : ",item_dict[item.lower()]-item_dict[item.lower()]*rand_disc1 + 1000)
+            elif ch2.lower() == "n":
+                print("Pay original price i.e : ",item_dict[item.lower()])
+                print("Thanks for shopping with us!")
+            else:
+                print("Invalid Input")
+        else:
+            print("Invalid Input")
+    elif ch.lower() == 'n':
+        print("Pay original price i.e : ",item_dict[item.lower()])
+        print("Thanks for shopping with us")
+    else:
+        print("Invalid input")
+else:
+    print("Item not found\nSorry for the inconvinience\nStock will be back soon")
